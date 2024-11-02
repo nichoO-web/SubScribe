de69ad7e0f5e7bb36f818177806a8c18c2ef8372
nichoOtero <nichonaks@gmail.com>
2024-11-02 03:37:12 -0500

    PORT

--- server.js
+++ server.js
@@ -52,4 +52,8 @@ app.use(isSignedIn);
 
 app.use('/users/:userId/subscriptions', subscriptionsController);
 
-app.listen('3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
